044037fab1464bff2b55347b14b33db38c476753
Hallie Parry <halloran.parry@gmail.com>
2015-06-28 17:31:51 -0400

    add broadcast event when timer expires

--- src/main/resources/assets/js/controllers.js
+++ src/main/resources/assets/js/controllers.js
@@ -1,6 +1,26 @@
-/*global angular*/
+/*globals angular, alert, console*/
 angular.module('StewardApp.controllers', []).controller('stewardController', function ($scope) {
-    $scope.add5Seconds = function () {
-                        $scope.$broadcast('timer-add-cd-seconds', 5);
-                    };
+    $scope.timerLength = 1500;
+    $scope.startTimer = function () {
+        $scope.$broadcast('timer-start');
+        $scope.timerRunning = true;
+    };
+
+    $scope.stopTimer = function () {
+        $scope.$broadcast('timer-stop');
+        $scope.timerRunning = false;
+    };
+
+    $scope.resetTimer = function () {
+        $scope.$broadcast('timer-reset');
+        $scope.timerRunning = false;
+    };
+
+    $scope.fin = function () {
+        console.log("FINISHED!");
+    };
+
+    $scope.$on('timer-finished', function () {
+        console.log('create step here');
+    });
 });
